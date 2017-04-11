e4461e803699953d43c1a2041c3b778951a3218e
alex nichols <alex nichols>
2017-04-11 12:42:15 -0700

    db quantity check/customer price update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ connection.connect(function(error){
 	}
 });
 
-function showResults(){
+function showStock(){
 	connection.query("SELECT * FROM products", function(error, results){
 		if(error){
 			console.log(error);
@@ -32,12 +32,13 @@ function showResults(){
 	});
 };
 
-function updateDatabase(){
-	connection.query("UPDATE products SET ?", function(error, response){
+function transaction(dbQuantity, customerPrice, productID){
+	connection.query("UPDATE products SET ? WHERE ?", {stock_quantity: dbQuantity}, {item_id: productID}, function(error, response){
 		if(error){
 			console.log(error);
 		}else{
-
+			console.log(response);
+			console.log("You owe $" + customerPrice);
 		}
 	})
 }
@@ -61,17 +62,31 @@ function userInput(){
 		var splitId = answer.id.split(":");
 		var getId = splitId[0];
 		console.log(getId);
-		var quantity = answer.quantity;
-		connection.query("SELECT ? FROM products", {item_id: getId}, function(error, results){
+		var userQuantity = answer.quantity;
+		connection.query("SELECT item_id, stock_quantity, price FROM products", function(error, results){
 			if(error){
 				console.log(error);
 			}else{
-				console.log(results);
-			}
+				for(var i = 0; i < results.length; i++){
+					if(getId == results[i].item_id && userQuantity < results[i].stock_quantity){
+						var productQuantity = results[i].stock_quantity;
+						var productPrice = results[i].price;
+						var userPrice = userQuantity * productPrice;
+						console.log("$" + userPrice);
+						var newQuantity = productQuantity - userQuantity;
+						console.log(newQuantity);
+						transaction(newQuantity, userPrice, getId);
+					};
+
+					if(getId == results[i].item_id && userQuantity > results[i].stock_quantity){
+						console.log("Insufficient Quantity");
+					};
+				};
+			};
 		});
 	});
-}
+};
 
-showResults();
+showStock();
 
 
