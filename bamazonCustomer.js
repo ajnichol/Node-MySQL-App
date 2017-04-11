a06d33ab9a29903a5253a771c6f033cbbe1cf396
alex nichols <alex nichols>
2017-04-11 14:53:00 -0700

    database query update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,15 +33,28 @@ function showStock(){
 };
 
 function transaction(dbQuantity, customerPrice, productID){
-	connection.query("UPDATE products SET ? WHERE ?", {stock_quantity: dbQuantity}, {item_id: productID}, function(error, response){
+	connection.query("UPDATE products SET ? WHERE item_id =" + productID, {stock_quantity: dbQuantity}, function(error, response){
 		if(error){
 			console.log(error);
 		}else{
-			console.log(response);
 			console.log("You owe $" + customerPrice);
-		}
-	})
-}
+			inquirer.prompt([
+			{
+				name: "continue",
+				type: "list",
+				message: "Would you like to continue shopping?",
+				choices: ["yes", "no"]
+			}
+			]).then(function(info){
+				if(info.continue == "yes"){
+					showStock();
+				}else{
+					console.log("Thanks for shopping at Alex's Emporium!")
+				};
+			});
+		};
+	});
+};
 
 function userInput(){
 	inquirer.prompt([
@@ -57,11 +70,8 @@ function userInput(){
 		message: "How many would you like to purchase?"
 	}
 	]).then(function(answer){
-		console.log(answer.id);
-		console.log(answer.quantity);
 		var splitId = answer.id.split(":");
 		var getId = splitId[0];
-		console.log(getId);
 		var userQuantity = answer.quantity;
 		connection.query("SELECT item_id, stock_quantity, price FROM products", function(error, results){
 			if(error){
@@ -72,14 +82,12 @@ function userInput(){
 						var productQuantity = results[i].stock_quantity;
 						var productPrice = results[i].price;
 						var userPrice = userQuantity * productPrice;
-						console.log("$" + userPrice);
 						var newQuantity = productQuantity - userQuantity;
-						console.log(newQuantity);
 						transaction(newQuantity, userPrice, getId);
 					};
 
 					if(getId == results[i].item_id && userQuantity > results[i].stock_quantity){
-						console.log("Insufficient Quantity");
+						console.log("We don't have enough in stock." + "\nPlease lower quantity.");
 					};
 				};
 			};
